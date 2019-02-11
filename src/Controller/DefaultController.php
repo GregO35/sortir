57f5713e52831c79c83bd3688835887df92f6e61
DUFOUR William <william.dufour2018@campus-eni.fr>
2019-02-11 17:29:12 +0100

    last commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/sortir/accueil", name="accueil", methods={"GET","POST"})
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+}

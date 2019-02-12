a9d82baaefda5e7103e907eb75f082ca3656ef20
DUFOUR William <william.dufour2018@campus-eni.fr>
2019-02-12 08:59:06 +0100

    change for login fonctionality

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -18,7 +18,7 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/login", name="app_login", methods={"GET","POST"})
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {

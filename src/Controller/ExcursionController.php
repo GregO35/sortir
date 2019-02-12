5fdd6392b86b7480aae3d25429bc4d36f02f803e
DUFOUR William <william.dufour2018@campus-eni.fr>
2019-02-12 17:03:15 +0100

    first step in creation excursion function

--- src/Controller/ExcursionController.php
+++ src/Controller/ExcursionController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Excursion;
+use App\Form\ExcursionType;
+use App\Repository\ExcursionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ExcursionController extends AbstractController
@@ -14,6 +18,32 @@ class ExcursionController extends AbstractController
     {
         return $this->render('user/accueil.html.twig', [
             'controller_name' => 'ExcursionController',
+        ]);
+    }
+
+    /**
+     * @Route("/sortir/sortie/creer", name="excursion_add")
+     */
+    public function addExcursion(Request $request)
+    {
+        $excursion = new Excursion();
+        $excursionForm = $this->createForm(ExcursionType::class, $excursion);
+        $excursionForm->handleRequest($request);
+
+        if($excursionForm->isSubmitted() && $excursionForm->isValid())
+        {
+            $excursion->setOrganizer($this->getUser());
+            $excursion->setState();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($excursion);
+            $em->flush();
+
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render("excursion/create.html.twig",[
+            'ecursionForm' => $excursionForm->createView()
         ]);
     }
 }

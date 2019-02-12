20b0421f8dffd78facf5f272cc7a1631c3e25258
STEPHAN GREGORY <gregory.stephan2018@campus-eni.fr>
2019-02-12 16:51:02 +0100

    findAll() excursion and view table all excursions

--- src/Controller/ExcursionController.php
+++ src/Controller/ExcursionController.php
@@ -2,18 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Excursion;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ExcursionController extends AbstractController
 {
+
     /**
-     * @Route("/accueil", name="excursion_list")
+     * @Route("/sortir/accueil", name="accueil", methods={"GET"})
      */
+    //requête pour afficher toutes les sorties (pour tester!) et les afficher (pour tester aussi!)
     public function listExcursion()
     {
-        return $this->render('user/accueil.html.twig', [
-            'controller_name' => 'ExcursionController',
+        $excursionRepository = $this
+            ->getDoctrine()
+            ->getRepository(Excursion::class);
+
+        $excursion = $excursionRepository->findAll();
+        //dd($excursion);
+        return $this->render('default/index.html.twig', [
+            "excursions"=>$excursion
         ]);
     }
 }
